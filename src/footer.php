56ed533ac594d4844e447b21524e4e155b24fc3a
RetroCraft <retrocraft314@gmail.com>
2017-03-15 15:13:24 -0400

    Nicer looking footer

--- src/footer.php
+++ src/footer.php
@@ -2,15 +2,16 @@
   </div> <!-- /not-footer -->
 
 
-  <footer class="bg-inverse">
+  <footer class="bg-inverse text-muted">
     <div class="container">
-      <?php if (is_active_sidebar('footer')): ?>
+      <!--<?php if (is_active_sidebar('footer')): ?>
         <div>
           <?php dynamic_sidebar('footer'); ?>
         </div>
-      <?php endif; ?>
+      <?php endif; ?>-->
+      <p>Made with &hearts; by James... I think. What's a &hearts; again?</p>
       <hr>
-      <p>&copy; James Ah Yong 2016.</p>
+      <small>Powered by Wordpress, Bootstrap, and a lot of <a href="//github.com/retrocraft/retrotheme">PHP</a>.</small>
     </div>
   </footer>
 

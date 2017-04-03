2cef2af569ec51f5ebaa20e68f6c2ad60f096c6b
RetroCraft <retrocraft314@gmail.com>
2017-04-03 19:53:18 -0400

    Some ads, finally

--- src/header.php
+++ src/header.php
@@ -17,6 +17,14 @@
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
 
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <script>
+    (adsbygoogle = window.adsbygoogle || []).push({
+      google_ad_client: "ca-pub-4770009561063252",
+      enable_page_level_ads: true
+    });
+  </script>
+
   <?php wp_head(); ?>
 </head>
 <body <?php body_class(); ?>>
--- src/sidebar.php
+++ src/sidebar.php
@@ -2,4 +2,15 @@
   <div>
     <?php dynamic_sidebar('post'); ?>
   </div>
+  <hr>
+  <script async src="//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+  <!-- Responsive Ad -->
+  <ins class="adsbygoogle"
+      style="display:block"
+      data-ad-client="ca-pub-4770009561063252"
+      data-ad-slot="2316025320"
+      data-ad-format="auto"></ins>
+  <script>
+  (adsbygoogle = window.adsbygoogle || []).push({});
+  </script>
 <?php endif; ?>

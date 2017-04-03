07f2b50e266be27c2eda57bdfa9bae6a0e99b955
RetroCraft <retrocraft314@gmail.com>
2017-04-03 19:41:02 -0400

    Fix navbar container

--- src/navbar.php
+++ src/navbar.php
@@ -1,20 +1,18 @@
     <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse" id="navbar">
-      <div class="container">
-        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <a class="navbar-brand" href="/">RetroCraft</a>
-        
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <?php            
-              wp_nav_menu(array(
-                'menu' => 'top_menu',
-                'container' => false,
-                'menu_class' => 'nav navbar-nav',
-                'depth' => 3,
-                'walker' => new wp_bootstrap_navwalker()
-              ));
-          ?>
-        </div>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <a class="navbar-brand" href="/">RetroCraft</a>
+      
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <?php            
+            wp_nav_menu(array(
+              'menu' => 'top_menu',
+              'container' => false,
+              'menu_class' => 'nav navbar-nav',
+              'depth' => 3,
+              'walker' => new wp_bootstrap_navwalker()
+            ));
+        ?>
       </div>
     </nav>

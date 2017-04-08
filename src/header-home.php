3b56d2b67b2a1b7648a8b540ec9a037988718c2d
RetroCraft <retrocraft314@gmail.com>
2017-04-08 19:29:43 -0400

    Cookie Consent

--- src/header-home.php
+++ src/header-home.php
@@ -13,6 +13,28 @@
     <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
   <![endif]-->
 
+  <link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.css" />
+  <script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.0.3/cookieconsent.min.js"></script>
+  <script>
+  window.addEventListener("load", function(){
+  window.cookieconsent.initialise({
+    "palette": {
+      "popup": {
+        "background": "#252e39"
+      },
+      "button": {
+        "background": "#14a7d0"
+      }
+    },
+    "content": {
+      "message": "This site uses these cool things called cookies. Apparently I'm supposed to tell you that because Google can track everything about you with them. Thanks Google!",
+      "dismiss": "Cool!",
+      "link": "Read a useless policy",
+      "href": "https://blog.retrocraft.ca/privacy-policy"
+    }
+  })});
+  </script>
+
   <?php wp_head(); ?>
 </head>
 <body> 
